c5122ee23da203d2a1d466cb6faca2ff84030e59
AgusFassola <agusfassolacarp@gmail.com>
2024-07-26 11:34:43 -0300

    pequeña validación

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,55 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const navigate = useNavigate();
+    const [ userName, setUserName ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        if( userName === 'admin' && password === 'admin' ||
+            userName === 'agus' && password === 'agus'
+        ){
+            alert(`Inicio Exitoso, Bienvenido/a ${userName} `)
+            navigate('/');
+        }else{
+            alert(`Usuario o contraseña incorrectos`)
+        }
+    }
+
     return (
         <div className='container mt-4'>
             <h2>Iniciar Sesión</h2>
-            <div>
-                <label>Usuario: </label>
-                <input
-                    placeholder='Usuario'
-                    type='text'
-                    className='form-control'
-                    required 
-                />
-            </div>
-            <div>
-                <label>Contraseña: </label>
-                <input
-                    placeholder='contraseña'
-                    type='password'
-                    className='form-control'
-                    required 
-                />
-            </div>
-            <button
-                className='btn btn-primary' type='submit'>Ingresar</button>
+            <form onSubmit={handleLogin}>
+                <div>
+                    <label>Usuario: </label>
+                    <input
+                        placeholder='Usuario'
+                        type='text'
+                        className='form-control'
+                        onChange={(e) => setUserName(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Contraseña: </label>
+                    <input
+                        placeholder='contraseña'
+                        type='password'
+                        className='form-control'
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button
+                    className='btn btn-primary' 
+                    type='submit'
+                    >Ingresar
+                </button>
+            </form>
+
         </div>
 
     );
